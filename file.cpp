2fd58206c622c02dd662fd87922b6e166959ed50
Maroš Matisko <matiskomaros@gmail.com>
2019-05-29 16:36:29 +0200

    fixed File exceptions and filename extraction

--- file.cpp
+++ file.cpp
@@ -10,7 +10,8 @@
 
 File::File(const std::string t_filePath) {
     size_t position = t_filePath.length() - 1;
-    while (t_filePath.at(position--) != '/') {
+    while (t_filePath.at(position) != '/') {
+        --position;
         if(position == 0) { break; }
     }
     m_fileName = t_filePath.substr(position + 1, t_filePath.length() - position - 1);
@@ -22,7 +23,7 @@ File::~File() {
     try {
         m_fileStream.close();
     } catch(const std::ifstream::failure& e) {
-        std::cout << "Exception while closing file: " << e.what() << std::endl;
+        throw std::runtime_error("Exception while closing file " + m_filePath + " : " + e.what() + "\n");
     }
     m_filePath.clear();
     m_fileName.clear();
@@ -32,7 +33,7 @@ void File::open() {
     try {
         m_fileStream.open(m_filePath);
     } catch(const std::ifstream::failure& e) {
-        std::cout << "Exception while opening file: " << e.what() << std::endl;
+        throw std::runtime_error("Exception while opening file " + m_filePath + " : " + e.what() + "\n");
     }
 }
 
@@ -46,7 +47,7 @@ char File::getNextChar(void) {
         try {
             m_fileStream.get(value);
         } catch(const std::ifstream::failure& e) {
-            std::cout << "Exception while reading file: " << e.what() << std::endl;
+            throw std::runtime_error("Exception while reading file " + m_filePath + " : " + e.what() + "\n");
         }
     }
     return value;
