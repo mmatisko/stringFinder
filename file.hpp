9ec51d304f35c343768abf2dc117368c7b6eb85f
Maroš Matisko <matiskomaros@gmail.com>
2019-06-11 00:24:37 +0200

    Changed member order in headers to have public first

--- file.hpp
+++ file.hpp
@@ -8,18 +8,6 @@
 
     namespace StringFinder {
         class File {
-        private:
-            std::string m_file_path;
-            std::string m_file_name;
-            std::ifstream m_file_stream;
-            short m_buffer_index = 1000;
-            constexpr static short BUFFER_LENGTH = 1000;
-            std::unique_ptr<char[]> m_read_buffer;
-
-        protected:
-			void close();
-            void cacheBuffer();
-
         public:
             explicit File(const std::string& t_file_path);
             ~File();
@@ -35,10 +23,21 @@
             // make class non-movable
             File(const File&& f) = delete;
             File& operator=(const File&& f) = delete;
+
+		protected:
+			void close();
+			void cacheBuffer();
+
+		private:
+			std::string m_file_path;
+			std::string m_file_name;
+			std::ifstream m_file_stream;
+			short m_buffer_index = 1000;
+			constexpr static int BUFFER_LENGTH = 1000;
+			std::unique_ptr<char[]> m_read_buffer;
         };
 
         typedef std::shared_ptr<File> FilePtr;
-        typedef std::queue<FilePtr> FileQueue;
     }
 
 #endif //FILE_HPP
--- filesystem.hpp
+++ filesystem.hpp
@@ -3,7 +3,7 @@
 
     #include <filesystem>
 
-    #include "file.hpp"
+    #include "filequeue.hpp"
 
 
     namespace StringFinder {
@@ -15,30 +15,30 @@
         
 
         class FileSystem {
+		public:
+			explicit FileSystem(const std::string& t_system_path);
+			~FileSystem();
+
+			bool pathIsValid() const;
+			FileQueue& getFiles();
+
+			void traversePath();
+			void processDirectory(const fs::path& t_directory_path);
+			void processFile(const fs::path& t_file_path);
+			void processPath(const fs::path& t_path);
+			bool traversalComplete() const;
+
+			// make class non-copyable
+			FileSystem(const FileSystem& f) = delete;
+			FileSystem& operator=(const FileSystem& f) = delete;
+			// make class non-movable
+			FileSystem(const FileSystem&& f) = delete;
+			FileSystem& operator=(const FileSystem&& f) = delete;
+
         private:
             fs::path m_system_path;
             FileQueue m_files;
-            bool m_traverse_complete;
-
-        public:
-            explicit FileSystem(const std::string& t_system_path);
-            ~FileSystem();
-
-            bool pathIsValid() const;
-            FileQueue& getFiles();
-            
-            void traversePath();
-            void processDirectory(const fs::path& t_directory_path);
-            void processFile(const fs::path& t_file_path);
-            void processPath(const fs::path& t_path);
-            bool traversalComplete() const;
-
-            // make class non-copyable
-            FileSystem(const FileSystem& f) = delete;
-            FileSystem& operator=(const FileSystem& f) = delete;
-            // make class non-movable
-            FileSystem(const FileSystem&& f) = delete;
-            FileSystem& operator=(const FileSystem&& f) = delete;
+            bool m_traverse_complete;    
         };
     }
 
